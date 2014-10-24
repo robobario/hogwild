ce727ba229a1cbc09da95728884d651e3883a815
roby <rob.young@adscale.co.nz>
2014-10-24 21:06:14 +1300

    handle paragraph formatting

--- src/main/resources/static/js/hogwild.js
+++ src/main/resources/static/js/hogwild.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     $.getJSON("/app/story", function (data) {
+        function toParagraphs(text){
+            var lines = text.split("\n");
+            var nonEmpty = [];
+            for (var i = 0; i < lines.length; i++) {
+                var line = lines[i];
+                if(line && line.trim() !== ""){
+                    nonEmpty.push(line.trim());
+                }
+            }
+            return nonEmpty;
+        }
+
         var entries = data.entries;
         for (var i = 0; i < entries.length; i++) {
             var entry = entries[i];
@@ -7,6 +19,13 @@ $(document).ready(function() {
             var heading = $("<h1>");
             var body = $("<body>");
             heading.text(entry.characterName);
+            var bodyParagraphs = toParagraphs(enty.body);
+            for (var j = 0; j < bodyParagraphs.length; j++) {
+                var paragraph = bodyParagraphs[j];
+                var p = $("<p>");
+                p.text(paragraph);
+                body.append(p);
+            }
             body.text(entry.body);
             container.append(heading);
             container.append(body);
